b46633ec1259e812ec68aaefd40f1ca8ad2e5455
Austin Smith <trunk9slamchest@gmail.com>
2019-10-08 17:17:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,10 +12,9 @@ class App < Sinatra::Base
   end
 
   get "/goodbye/:name" do
-  	@name = params[:name]
-  	"Goodbye, #{@name}."
+    @user_name = params[:name]
+    "Goodbye #{@user_name}."
   end
-
 
   get "/multiply/:num1/:num2" do
     @product = params[:num1].to_i * params[:num2].to_i
